f01b94234d9cd4951b3bc170ab537714a9616bc5
gulmatovdilbek <dgulmatov16@gmail.com>
2026-04-11 18:05:39 +0500

    first commit

---
+++ 5.py
@@ -0,0 +1,15 @@
+# n = int(input())
+# s = 0
+# for son in range(1, n + 1):
+#     s += son # s = s + son
+
+# print(s)
+
+
+import math
+
+n = int(input())
+s = 0
+for son in range(1,n + 1):
+    s += int(math.sqrt(son))
+print(s)
